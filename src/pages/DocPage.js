1adc37e00126eb5c73021684d6d2e3b8fa7682cb
Yiqun WANG <wyq@umich.edu>
2024-12-18 18:54:24 -0600

    Styles table in doc page

--- src/pages/DocPage.js
+++ src/pages/DocPage.js
@@ -6,6 +6,7 @@ import { materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import docContent from '../mdFiles/Doc.md';
 import NavBar from '../NavBar';
 import 'github-markdown-css';
+import remarkGfm from 'remark-gfm'
 
 function DocPage() {
   const [content, setContent] = useState('');
@@ -21,8 +22,9 @@ function DocPage() {
     <div className="App">
       <NavBar />
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Box sx={{ p: 4 }}>
+        <Box sx={{ p: 4, textAlign: 'left' }} className={'markdown-body'}>
           <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
             components={{
               code({node, inline, className, children, ...props}) {
                 const match = /language-(\w+)/.exec(className || '');
