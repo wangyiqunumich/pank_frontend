4e49e5fb75cfa128d8c17bd8af3beeadcdf3501b
Yiqun WANG <wyq@umich.edu>
2024-12-23 08:58:30 -0600

    Add copy paste button

--- src/pages/ApiPage.js
+++ src/pages/ApiPage.js
@@ -6,6 +6,35 @@ import { materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import apiContent from '../schema/doc/API.md';
 import NavBar from '../NavBar';
 import 'github-markdown-css';
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import './ApiPage.css'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+
+export function CodeCopyBtn({ children }) {
+    const [copyOk, setCopyOk] = React.useState(false);
+
+    const iconColor = copyOk ? '#0af20a' : '#ddd';
+    const icon = copyOk ? 'fa-check-square' : 'fa-copy';
+
+    const handleClick = (e) => {
+        navigator.clipboard.writeText(children.props.children);
+        console.log(children)
+
+        setCopyOk(true);
+        setTimeout(() => {
+            setCopyOk(false);
+        }, 5000);
+    }
+
+    return (
+        <div className="code-copy-btn" style={{ width: '20px', height: '20px', zIndex: 1}}>
+            <i className={`fas ${icon}`} onClick={handleClick} style={{color: 'black', width: '20px', height: '20px', zIndex: 1}}>
+                <ContentCopyIcon/>
+            </i>
+        </div>
+    )
+}
 
 function ApiPage() {
   const [content, setContent] = useState('');
@@ -16,6 +45,10 @@ function ApiPage() {
       .then(text => setContent(text))
       .catch(error => console.error('Error loading API documentation:', error));
   }, []);
+    const Pre = ({ children }) => <pre className="blog-pre">
+        <CodeCopyBtn>{children}</CodeCopyBtn>
+        {children}
+    </pre>
 
   return (
     <div className="App">
@@ -23,7 +56,12 @@ function ApiPage() {
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
         <Box sx={{ p: 4, textAlign: 'left' }} className={'markdown-body'}>
           <ReactMarkdown
+              className={'post-markdown'}
+              // linkTarget='_blank'
+              rehypePlugins={[rehypeRaw]}
+              remarkPlugins={[remarkGfm]}
             components={{
+                pre: Pre,
               code({node, inline, className, children, ...props}) {
                 const match = /language-(\w+)/.exec(className || '');
                 return !inline && match ? (
