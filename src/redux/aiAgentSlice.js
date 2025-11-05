fc06ff05422ed63adcc82992ba8e65be17d530e4
Bing Han <2082979557@qq.com>
2025-11-04 23:12:13 -0500

    fix

--- src/redux/aiAgentSlice.js
+++ src/redux/aiAgentSlice.js
@@ -1,49 +1,62 @@
-import {
-  createAsyncThunk,
-  createSlice,
-} from '@reduxjs/toolkit';
-import { QueryStatus } from '@reduxjs/toolkit/query';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { QueryStatus } from "@reduxjs/toolkit/query";
+import { flaskBackendAxiosInstanceNew } from "../axios/axios";
 
-import { flaskBackendAxiosInstanceNew } from '../axios/axios';
+export const queryAiAgent = createAsyncThunk(
+  "/aiAgent",
+  async (payload, { signal, rejectWithValue }) => {
+    // Create a controller for Axios
+    const controller = new AbortController();
 
-export const queryAiAgent = createAsyncThunk('/aiAgent',
-    async (payload) => {
-        return await flaskBackendAxiosInstanceNew
-            .post('/pank3-ai-agent', payload, {
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            })
-            .then((response) => response.data)
-            .catch((response) => {
-                console.log(response);
-            });
-    });
+    // Connect Redux Toolkit's signal to Axios' AbortController
+    signal.addEventListener("abort", () => controller.abort());
+
+    try {
+      const response = await flaskBackendAxiosInstanceNew.post(
+        "/pank3-ai-agent",
+        payload,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          signal: controller.signal, // attach abort signal
+        }
+      );
+
+      return response.data;
+    } catch (error) {
+      if (error.name === "CanceledError" || error.code === "ERR_CANCELED") {
+        // Axios throws a CanceledError when aborted
+        return rejectWithValue("Request canceled by user");
+      }
+      console.error(error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 export const aiAgentSlice = createSlice({
-    name: 'aiAgent',
-    initialState: {
-        aiAgent: {},
-        queryAiAgentStatus: QueryStatus.uninitialized, // This is auto updated
-        queryAiAgentErrorMessage: ''
-    },
-    // reducers: {}
-    extraReducers: (builder) => {
-        builder
-            .addCase(queryAiAgent.pending, (state) => {
-                state.queryAiAgentStatus = QueryStatus.pending;
-            })
-            .addCase(queryAiAgent.fulfilled, (state, action) => {
-                state.aiAgent = action.payload;
-                state.queryAiAgentStatus = QueryStatus.fulfilled;
-            })
-            .addCase(queryAiAgent.rejected, (state, action) => {
-                state.queryAiAgentErrorMessage = action.error.message;
-                state.queryAiAgentStatus = QueryStatus.rejected;
-            });
-    }
+  name: "aiAgent",
+  initialState: {
+    aiAgent: {},
+    queryAiAgentStatus: QueryStatus.uninitialized,
+    queryAiAgentErrorMessage: "",
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(queryAiAgent.pending, (state) => {
+        state.queryAiAgentStatus = QueryStatus.pending;
+        state.queryAiAgentErrorMessage = "";
+      })
+      .addCase(queryAiAgent.fulfilled, (state, action) => {
+        state.aiAgent = action.payload;
+        state.queryAiAgentStatus = QueryStatus.fulfilled;
+      })
+      .addCase(queryAiAgent.rejected, (state, action) => {
+        state.queryAiAgentStatus = QueryStatus.rejected;
+        state.queryAiAgentErrorMessage = action.payload || action.error.message;
+      });
+  },
 });
-
-// export const {} = aiAgentSlice.actions;
 
 export default aiAgentSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import aiAgentReducer from './aiAgentSlice';
 import aiAnswerReducer from './aiAnswerSlice';
 import articlesReducer from './articlesSlice';
-import catalogReducer from './catalogSlice';
+// import catalogReducer from './catalogSlice';
 import feedbackReducer from './feedbackSlice';
 import graphviewerReducer from './graphviewerSlice';
 import inputToVocabReducer from './inputToVocabSlice';
 import processedQuestionReducer from './processedQuestionSlice';
 import queryResultPage from './queryResultPage';
 import queryResultReducer from './queryResultSlice';
-import queryVisResultReducer from './queryVisResultSlice';
+// import queryVisResultReducer from './queryVisResultSlice';
 import searchReducer from './searchSlice';
 import supportingMaterialReducer from './supportingMaterialSlice';
 import typeToImageReducer from './typeToImageSlice';
@@ -20,7 +20,7 @@ import viewSchemaReducer from './viewSchemaSlice';
 const store = configureStore({
     reducer: {
         viewSchema: viewSchemaReducer,
-        catalog: catalogReducer,
+        // catalog: catalogReducer,
         queryResult: queryResultReducer,
         aiAnswer: aiAnswerReducer,
         articles: articlesReducer,
@@ -29,7 +29,7 @@ const store = configureStore({
         inputToVocab: inputToVocabReducer,
         search: searchReducer,
         variables: variablesReducer,
-        queryVisResult: queryVisResultReducer,
+        // queryVisResult: queryVisResultReducer,
         queryResultPage: queryResultPage,
         feedback: feedbackReducer,
         graphviewer: graphviewerReducer,
