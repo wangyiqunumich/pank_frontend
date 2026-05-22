53fc0dd8a2fa9832d9ad4df0cedf735c8fe5939f
hb2022 <hb2022@sjtu.edu.cn>
2026-05-21 20:05:00 -0400

    fixes

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,3 +1,3 @@
-// export const PLANNER_AGENT_BASE_URL = 'https://jieliulab3.dcmb.med.umich.edu/pankgraph-agent';
-export const PLANNER_AGENT_BASE_URL = 'https://pankgraph-ai-agent-gpu-manually-381685410.us-east-1.elb.amazonaws.com';
+export const PLANNER_AGENT_BASE_URL = 'https://jieliulab3.dcmb.med.umich.edu/pankgraph-agent';
+// export const PLANNER_AGENT_BASE_URL = 'https://pankgraph-ai-agent-gpu-manually-381685410.us-east-1.elb.amazonaws.com';
 export const STREAM_AGENT_BASE_URL = 'https://agent.pankgraph.org';
